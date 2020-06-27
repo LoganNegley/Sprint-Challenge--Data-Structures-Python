2e49c472e74a26e52ed55e88abc681b857790c1e
Logan Negley <logannegley@gmail.com>
2020-06-26 19:14:08 -0500

    fixed insert bug

--- names/names.py
+++ names/names.py
@@ -11,13 +11,36 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# Initial start of the tree 
+bsTree = BSTNode('names') 
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+
+
+# Whats need?
+# use binary tree initialize the tree with string value
+# use binary insert to add names to tree
+# insert each name in the tree
+# if value of name1 contains value of name2 add to duplicates list
+
+# For every name in names_1 put into tree
+for n in names_1: 
+    bsTree.insert(n)
+# For everyname in names_2 put into tree
+for n2 in names_2:
+    bsTree.insert(n2)
+    # if statement here name1 and name2 are the same add it to duplicates list
+
+
+
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
+
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
